d04020e8aa728c19d09a712738975a1d236b2bf5
Ben Zeng <benzeng94@me.com>
2019-11-26 18:23:01 +0000

    store current pageRef and bearListUpdate event

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,5 +1,5 @@
-import { NavigationMixin } from 'lightning/navigation';
-
+import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
+import { fireEvent } from 'c/pubsub';
 import { LightningElement, track, wire } from 'lwc';
 import { loadStyle } from 'lightning/platformResourceLoader';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
@@ -7,8 +7,16 @@ import ursusResources from '@salesforce/resourceUrl/ursus_park';
 import searchBears from '@salesforce/apex/BearController.searchBears';
 export default class BearList extends NavigationMixin(LightningElement) {
 	@track searchTerm = '';
-	@wire(searchBears, {searchTerm: '$searchTerm'})
-	bears;
+    
+    @track bears;
+    @wire(CurrentPageReference) pageRef;
+    @wire(searchBears, {searchTerm: '$searchTerm'})
+    loadBears(result) {
+        this.bears = result;
+        if (result.data) {
+            fireEvent(this.pageRef, 'bearListUpdate', result.data);
+        }
+    }
     
     connectedCallback() {
         loadStyle(this, ursusResources + '/style.css');
