ce57a68c79ce66074d4465922a0eeacf10b7cd81
Ben Zeng <benzeng94@me.com>
2019-11-26 15:47:42 +0000

    hello worlding now in card title, and body shows current date

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -4,4 +4,9 @@ export default class HelloWebComponent extends LightningElement {
     handleGreetingChange(event) {
         this.greeting = event.target.value;
     }
+
+    currentDate = new Date().toDateString();
+    get capitalizedGreeting() {
+	return `Hello ${this.greeting.toUpperCase()}!`;
+    }      
 }
