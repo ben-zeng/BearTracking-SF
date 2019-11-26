dd8f7c6de6b59bea0612836b484b7fa461014dd3
Ben Zeng <benzeng94@me.com>
2019-11-26 18:17:53 +0000

    class extends NavMixIn applied to lightning element base class

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -5,7 +5,7 @@ import { loadStyle } from 'lightning/platformResourceLoader';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 /** BearController.searchBears(searchTerm) Apex method */
 import searchBears from '@salesforce/apex/BearController.searchBears';
-export default class BearList extends LightningElement {
+export default class BearList extends NavigationMixin(LightningElement) {
 	@track searchTerm = '';
 	@wire(searchBears, {searchTerm: '$searchTerm'})
 	bears;
